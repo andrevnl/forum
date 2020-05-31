0fe33824d60ee14290c7a57cb4143af5cb8348a1
andrevnl <andrevnl@hotmail.com>
2020-05-31 00:26:24 -0300

    Utilizando o padrão Repository do Spring Boot e herdando de uma interface que já tem os metodos mais comuns pra serem usados de forma bem mais facil do que pelo DTO

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,21 +1,23 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista() {
-        Topico topico = new Topico("Dúvida", "Dúvida de Spring",
-                new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
     }
 }
