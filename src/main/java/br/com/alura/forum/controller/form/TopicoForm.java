7a14e8102feab345065d3faaadfc335c8689430a
andrevnl <andrevnl@hotmail.com>
2020-06-02 22:47:33 -0300

    Finalizando metodo cadastrar, corrigindo nome do campo no findBy

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -35,7 +35,7 @@ public class TopicoForm {
     }
 
     public Topico converter(CursoRepository cursoRepository) {
-        Curso curso = cursoRepository.findByName(nomeCurso);
+        Curso curso = cursoRepository.findByNome(nomeCurso);
         return new Topico(titulo, mensagem, curso);
     }
 }
