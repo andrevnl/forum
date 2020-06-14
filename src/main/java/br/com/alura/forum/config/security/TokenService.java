268ecf739ed9bffca354602ea9e9568ee9347c53
andrevnl <andrevnl@hotmail.com>
2020-06-14 13:24:31 -0300

    Autenticando cada requisicao mandada usando o Filter com a Autenticacao JWT

--- src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -1,8 +1,13 @@
 package br.com.alura.forum.config.security;
 
 import br.com.alura.forum.model.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -28,5 +33,19 @@ public class TokenService {
                 .setExpiration(dataExpiracao)
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
+    }
+
+    public boolean isTokenValido(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Long getIdUsuario(String token) {
+        Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
     }
 }
